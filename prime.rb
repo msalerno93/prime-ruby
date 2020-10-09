6247f0a4961db82c837ab3cb42a496757c2e1da8
msalerno93 <msalerno1993@gmail.com>
2020-10-09 17:56:49 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+    if number <= 1
+        return false
+    end
+    (2..(number - 1)).each do |num|
+        if number % num == 0
+            return false
+        end
+    end
+    return true
+end
